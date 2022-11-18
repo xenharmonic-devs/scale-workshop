a7428a55c610046242c58b6971f852c523aafc8e
Lumi Pakkanen <lumi.pakkanen@gmail.com>
2022-11-18 16:47:22 +0200

    Include user-submitted Scala test scales
    ref #339

--- src/importers/__tests__/scala.spec.ts
+++ src/importers/__tests__/scala.spec.ts
@@ -1,6 +1,10 @@
 import { describe, it, expect } from "vitest";
 
 import { ScalaImporter } from "../scala";
+// @ts-ignore
+import SHANG_TEST_SCALE from "./shang_pentatonic.scl?raw";
+// @ts-ignore
+import ZHI_TEST_SCALE from "./zhi_pentatonic.scl?raw";
 
 describe("Scala importer", () => {
   it("can handle all line types", () => {
@@ -34,5 +38,21 @@ describe("Scala importer", () => {
     expect(scale.getMonzo(4).valueOf()).toBe(1.25);
 
     expect(scale.getMonzo(12).valueOf()).toBe(2);
+  });
+
+  it("can handle the shang user test scale", () => {
+    const importer = new ScalaImporter();
+    const { scale } = importer.parseText(SHANG_TEST_SCALE);
+
+    expect(scale.getMonzo(1).valueOf()).toBeCloseTo(1.125);
+    expect(scale.size).toBe(5);
+  });
+
+  it("can handle the zhi user test scale", () => {
+    const importer = new ScalaImporter();
+    const { scale } = importer.parseText(ZHI_TEST_SCALE);
+
+    expect(scale.getMonzo(3).valueOf()).toBeCloseTo(1.5);
+    expect(scale.size).toBe(5);
   });
 });
