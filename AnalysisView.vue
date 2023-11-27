89067a2df000437bfa7047ef23932ee38a06c6b4
inthar-raven <36112167+inthar-raven@users.noreply.github.com>
2023-11-27 15:28:18 -0500

    Add files via upload
    Added reactivity for Issue #411.

---
+++ AnalysisView.vue
@@ -0,0 +1,311 @@
+<script setup lang="ts">
+import { intervalMatrix } from "@/analysis";
+import type { VirtualSynth } from "@/virtual-synth";
+import ChordWheel from "@/components/ChordWheel.vue";
+import { computed, ref } from "vue";
+import {
+  reverseParseInterval,
+  type Interval,
+  type IntervalOptions,
+  type Scale,
+} from "scale-workshop-core";
+
+const MAX_SCALE_SIZE = 100;
+
+const props = defineProps<{
+  scale: Scale;
+  virtualSynth: VirtualSynth;
+  colorScheme: "light" | "dark";
+}>();
+
+const cellFormat = ref<"best" | "cents" | "decimal">("best");
+const indexing = ref<"zero" | "one">("zero");
+const matrixStartIndex = computed(() => indexing.value === "one" ? 1 : 0); 
+const trailLongevity = ref(70);
+const maxOtonalRoot = ref(16);
+const maxUtonalRoot = ref(23);
+
+const fadeAlpha = computed(() => 1 - trailLongevity.value / 100);
+
+const backgroundRBG = computed<[number, number, number]>(() => {
+  // Add dependency.
+  props.colorScheme;
+  // Fetch from document.
+  const css = getComputedStyle(document.documentElement)
+    .getPropertyValue("--color-background")
+    .trim()
+    .toLowerCase();
+  if (css === "#fff") {
+    return [255, 255, 255];
+  } else if (css === "#000") {
+    return [0, 0, 0];
+  } else {
+    throw new Error("General color parsing not implemented");
+  }
+});
+
+const strokeStyle = computed(() => {
+  // Add dependency.
+  props.colorScheme;
+  // Fetch from document.
+  return getComputedStyle(document.documentElement)
+    .getPropertyValue("--color-text")
+    .trim();
+});
+
+// While interval.name suffices for the tuning table
+// we want more accurate results here.
+function formatMatrixCell(interval: Interval) {
+  // We don't have much space so let's not waste it on insignificant digits.
+  const options: IntervalOptions = {
+    centsFractionDigits: 1,
+    decimalFractionDigits: 3,
+  };
+  interval = interval.mergeOptions(options);
+
+  if (cellFormat.value === "cents") {
+    return interval.centsString();
+  }
+  if (cellFormat.value === "decimal") {
+    // Consistent with tuning table localization.
+    return interval.decimalString().replace(",", ".");
+  }
+
+  // Monzos are too long.
+  if (interval.type === "monzo") {
+    return interval.centsString();
+  }
+  // Composite intervals are too long or not accurate.
+  if (interval.isComposite()) {
+    return interval.centsString();
+  }
+  // If we're here the name should faithfully represent the interval.
+  // Reverse parsing takes care of obscure corner cases.
+  return reverseParseInterval(interval);
+}
+
+const matrix = computed(() => {
+  return intervalMatrix(
+    props.scale.head(MAX_SCALE_SIZE).mergeOptions({
+      centsFractionDigits: 1,
+      decimalFractionDigits: 3,
+    }), 
+    1 - (matrixStartIndex.value)
+  ).map((row) => row.map(formatMatrixCell));
+});
+</script>
+
+<template>
+  <main>
+    <h2>Interval matrix (modes)</h2>
+    <div class="control-group interval-matrix">
+      <table>
+        <tr>
+          <th></th>
+          <th v-for="i of Math.min(scale.size-1+matrixStartIndex, MAX_SCALE_SIZE-1)" :key="i">
+            {{ i }}
+          </th>
+          <th>({{ scale.size + matrixStartIndex }})</th>
+        </tr>
+        <tr v-for="(row, i) of matrix" :key="i">
+          <th>{{ formatMatrixCell(scale.getInterval(i)) }}</th>
+          <td v-for="(name, j) of row" :key="j">{{ name }}</td>
+        </tr>
+      </table>
+    </div>
+    <div class="control-group">
+      <div class="control radio-group">
+        <label>Display intervals in matrix as</label>
+        <span>
+          <input
+            type="radio"
+            id="format-best"
+            value="best"
+            v-model="cellFormat"
+          />
+          <label for="format-best"> Default </label>
+        </span>
+
+        <span>
+          <input
+            type="radio"
+            id="format-cents"
+            value="cents"
+            v-model="cellFormat"
+          />
+          <label for="format-cents"> Cents </label>
+        </span>
+
+        <span>
+          <input
+            type="radio"
+            id="format-decimal"
+            value="decimal"
+            v-model="cellFormat"
+          />
+          <label for="format-decimal"> Decimal ratio </label>
+        </span>
+      </div>
+      <div class="control radio-group">
+        <label>Interval indexing</label>
+        <span>
+          <input
+            type="radio"
+            class="radio"
+            id="indexing-zero"
+            value="zero"
+            v-model="indexing"
+          />
+          <label for="indexing-zero"> 0-indexing (default) </label>
+        </span>
+        
+        <span>
+          <input
+            type="radio"
+            id="indexing-one"
+            value="one"
+            v-model="indexing"
+          />
+          <label for="indexing-one"> 1-indexing </label>
+        </span>
+      </div>
+    </div>
+    <div class="columns-container">
+      <div class="column">
+        <h2>Otonal chord</h2>
+        <div class="control-group">
+          <ChordWheel
+            class="chord-wheel"
+            type="otonal"
+            :maxChordRoot="maxOtonalRoot"
+            :virtualSynth="virtualSynth"
+            :width="500"
+            :height="400"
+            :lineWidth="2"
+            :backgroundRBG="backgroundRBG"
+            :fadeAlpha="fadeAlpha"
+            :shadowBlur="2"
+            :strokeStyle="strokeStyle"
+            textColor="red"
+          />
+        </div>
+      </div>
+      <div class="column">
+        <h2>Utonal chord</h2>
+        <div class="control-group">
+          <ChordWheel
+            class="chord-wheel"
+            type="utonal"
+            :maxChordRoot="maxUtonalRoot"
+            :virtualSynth="virtualSynth"
+            :width="500"
+            :height="400"
+            :lineWidth="2"
+            :backgroundRBG="backgroundRBG"
+            :fadeAlpha="fadeAlpha"
+            :shadowBlur="2"
+            :strokeStyle="strokeStyle"
+            textColor="blue"
+          />
+        </div>
+      </div>
+      <div class="column">
+        <h2>Chord analysis</h2>
+        <div class="control-group">
+          <div class="control">
+            <label for="trail-longevity">Trail longevity</label>
+            <input
+              id="trail-longevity"
+              type="number"
+              class="control"
+              min="0"
+              max="100"
+              v-model="trailLongevity"
+            />
+          </div>
+        </div>
+        <div class="control-group">
+          <div class="control">
+            <label for="otonal-root">Maximum root (otonal)</label>
+            <input
+              id="otonal-root"
+              type="number"
+              class="control"
+              min="1"
+              v-model="maxOtonalRoot"
+            />
+          </div>
+        </div>
+        <div class="control-group">
+          <div class="control">
+            <label for="utonal-root">Maximum root (utonal)</label>
+            <input
+              id="utonal-root"
+              type="number"
+              class="control"
+              min="1"
+              v-model="maxUtonalRoot"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+/* View */
+main {
+  padding: 1rem;
+  overflow-y: auto !important;
+}
+
+/* Interval matrix */
+.interval-matrix {
+  overflow-x: auto;
+  display: block;
+}
+.interval-matrix th {
+  font-weight: bold;
+}
+.interval-matrix table,
+.interval-matrix th,
+.interval-matrix td {
+  border: 1px solid var(--color-border);
+}
+.interval-matrix th,
+.interval-matrix td {
+  padding: 0.2rem 0.5rem;
+}
+.interval-matrix table {
+  border-collapse: collapse;
+  text-align: center;
+}
+
+/* Content layout (medium) */
+div.columns-container {
+  column-count: 2;
+  column-gap: 1rem;
+  overflow: hidden;
+}
+div.column {
+  overflow-y: auto;
+}
+
+/* Content layout (medium and large) */
+@media screen and (min-width: 600px) {
+  div.columns-container {
+    column-count: 3;
+  }
+  div.column {
+    height: 100%;
+  }
+}
+
+/* Chord wheel */
+.chord-wheel {
+  border: 1px solid var(--color-border);
+  max-width: 100%;
+  height: auto;
+}
+</style>
---
+++ analysis.ts
@@ -0,0 +1,114 @@
+import type { Scale } from "scale-workshop-core";
+
+const EPSILON = 1e-6;
+
+// Absolute logarithmic error from the nearest harmonic measured in nats
+function harmonicError(ratio: number) {
+  return Math.abs(Math.log(ratio) - Math.log(Math.round(ratio)));
+}
+
+// Calculate a small correction that removes the bias
+// in the movement of an otonal chord wheel.
+function otonalCorrection(ratios: number[]) {
+  let correction = 1;
+  ratios.forEach((ratio) => {
+    correction *= ratio / Math.round(ratio);
+  });
+
+  return Math.pow(correction, -1 / ratios.length);
+}
+
+// Calculate the best multiplier for a set of ratios that
+// miminizes the perceived motion of an otonal chord wheel.
+function otonalMultiplier(ratios: number[], maxMultiplier = 16) {
+  let leastError = Infinity;
+  let result = 1;
+  for (let multiplier = 1; multiplier <= maxMultiplier; ++multiplier) {
+    const candidate =
+      multiplier * otonalCorrection(ratios.map((ratio) => ratio * multiplier));
+    const balanced = ratios.map((ratio) => ratio * candidate);
+    const error = balanced.map(harmonicError).reduce((a, b) => a + b);
+    if (error + EPSILON < leastError) {
+      leastError = error;
+      result = candidate;
+    }
+  }
+  return result;
+}
+
+/**
+ * Calculate the fundamental frequency implied by an array of frequencies
+ * interpreted as an enumerated otonal chord.
+ * @param frequencies Array of frequencies in the chord with the root first.
+ * @param maxMultiplier Maximum interpretation of the root as an integer in an enumerated chord.
+ * @returns The fundamental frequency below the root that minimizes the perceived motion of an otonal chord wheel.
+ */
+export function otonalFundamental(frequencies: number[], maxMultiplier = 16) {
+  if (!frequencies.length) {
+    return NaN;
+  }
+  const ratios = frequencies.map((f) => f / frequencies[0]);
+  const multiplier = otonalMultiplier(ratios, maxMultiplier);
+  return frequencies[0] / multiplier;
+}
+
+// Absolute logarithmic error from the nearest subharmonic measured in nats
+function subharmonicError(ratio: number) {
+  return Math.abs(Math.log(ratio) + Math.log(Math.round(1 / ratio)));
+}
+
+// Calculate a small correction that removes the bias
+// in the movement of an utonal chord wheel.
+function utonalCorrection(ratios: number[]) {
+  let correction = 1;
+  ratios.forEach((ratio) => {
+    correction *= ratio * Math.round(1 / ratio);
+  });
+
+  return Math.pow(correction, 1 / ratios.length);
+}
+
+// Calculate the best divisor for a set of ratios that
+// miminizes the perceived motion of an utonal chord wheel.
+export function utonalDivisor(ratios: number[], maxDivisor = 23) {
+  let leastError = Infinity;
+  let result = 1;
+  for (let divisor = 1; divisor <= maxDivisor; ++divisor) {
+    const candidate =
+      divisor * utonalCorrection(ratios.map((ratio) => ratio / divisor));
+    const balanced = ratios.map((ratio) => ratio / candidate);
+    const error = balanced.map(subharmonicError).reduce((a, b) => a + b);
+    if (error + EPSILON < leastError) {
+      leastError = error;
+      result = candidate;
+    }
+  }
+  return result;
+}
+
+/**
+ * Calculate the (high) fundamental frequency implied by an array of frequencies
+ * interpreted as an enumerated utonal (inverted) chord.
+ * @param frequencies Array of frequencies in the chord with the root first.
+ * @param maxDivisor Maximum interpretation of the root as an integer in an enumerated chord.
+ * @returns The fundamental frequency above the root that minimizes the perceived motion of an utonal chord wheel.
+ */
+export function utonalFundamental(frequencies: number[], maxDivisor = 23) {
+  if (!frequencies.length) {
+    return NaN;
+  }
+  const ratios = frequencies.map((f) => f / frequencies[0]);
+  const divisor = utonalDivisor(ratios, maxDivisor);
+  return divisor * frequencies[0];
+}
+
+// Interval matrix a.k.a the modes of a scale
+export function intervalMatrix(scale: Scale, startIndex: number) {
+  const result = [];
+  const degrees = [...Array(scale.size + 1 - startIndex).keys()];
+  for (let i = 0; i < scale.size; ++i) {
+    const mode = scale.rotate(i);
+    result.push(degrees.map((j) => mode.getInterval(j + startIndex)));
+  }
+  return result;
+}
