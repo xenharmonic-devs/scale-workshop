448cff409ac15e5da4c0fc23ed930967fbd368b3
Lumi Pakkanen <lumi.pakkanen@gmail.com>
2023-11-30 20:22:02 +0200

    Fix base frequency encoding in URL
    Access a reactive property to trigger the watcher.
    Implement Cypress test to verify the fix.
    
    ref #508

--- cypress/e2e/basic.cy.ts
+++ cypress/e2e/basic.cy.ts
@@ -5,6 +5,18 @@ describe("Basic test", () => {
     cy.visit("/");
     cy.contains("h2", "Scale data");
   });
+
+  it("preserves the base frequency when changing tabs", () => {
+    cy.visit("/");
+    cy.get(".real-valued").clear()
+    cy.get(".real-valued").type("432")
+    cy.get(".real-valued").trigger("change")
+    cy.get(".real-valued").should("have.value", "432");
+    // eslint-disable-next-line cypress/no-unnecessary-waiting
+    cy.wait(400); // Wait for debounce to expire.
+    cy.get("a").contains("Synth").click();
+    cy.url().should("contain", "f=");
+  });
 });
 
 describe("404 page", () => {
