c38f3b66dcbf4dfcb81291a73aa66a7785b1e51c
Marc Sabat <masa@plainsound.org>
2024-04-16 18:56:36 +0200

    fixing MidiView

--- src/views/MidiView.vue
+++ src/views/MidiView.vue
@@ -167,7 +167,7 @@ onUnmounted(() => {
           <div class="control channels-wrapper">
             <label>Input channels</label>
             <span v-for="channel in 16" :key="channel">
-              <label :class="{ active: inputHighlights.has(channel) }">{{channel}}</label>
+              <label :class="{ active: inputHighlights.has(channel) }">{{ channel }}</label>
               <input
                 type="checkbox"
                 :value="channel"
@@ -232,7 +232,7 @@ onUnmounted(() => {
           <div class="control channels-wrapper">
             <label>Output channels</label>
             <span v-for="channel in 16" :key="channel">
-              <label>{{ ' ' + channel + ' ' }}</label>
+              <label>{{ channel }}</label>
               <input
                 type="checkbox"
                 :value="channel"
