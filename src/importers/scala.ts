d0dfb268f01a2b532bbeb1f2bb39e79c4c33d161
Lumi Pakkanen <lumi.pakkanen@gmail.com>
2024-05-21 19:46:21 +0300

    Fix Scala .scl import line inference
    Also import .scl scale title.
    
    ref #706

--- src/importers/scala.ts
+++ src/importers/scala.ts
@@ -4,22 +4,38 @@ import { TextImporter, type ImportResult } from '@/importers/base'
 export class ScalaImporter extends TextImporter {
   parseText(input: string): ImportResult {
     const lines = input.split(NEWLINE_TEST)
-    const firstLine = lines.lastIndexOf('!') + 1
+    let validLineCount = 0
+    let name = ''
     const sourceLines: string[] = []
-    lines.slice(firstLine).forEach((line) => {
+    for (let line of lines) {
       line = line.trim()
+      // Ignore comments
+      if (line.startsWith('!')) {
+        continue
+      }
+      validLineCount++
+      // Empty lines are valid, but not processed
       if (!line.length) {
-        return
+        continue
       }
-      const parts = line.split(/\s/)
-      if (parts.length === 1) {
-        // Valid .scl is valid SonicWeave
-        sourceLines.push(parts[0])
-      } else if (parts.length > 1) {
-        // Unofficially labeled .scl is valid SonicWeave if quoted
-        sourceLines.push(parts[0] + ' ' + JSON.stringify(parts.slice(1).join('')))
+      // Store scale title
+      if (validLineCount === 1) {
+        name = line
       }
-    })
-    return { sourceText: sourceLines.join('\n') }
+      // validLineCount === 2: Ignore number of notes
+
+      // Process true lines
+      if (validLineCount > 2) {
+        const parts = line.split(/\s/)
+        if (parts.length === 1) {
+          // Valid .scl is valid SonicWeave
+          sourceLines.push(parts[0])
+        } else if (parts.length > 1) {
+          // Unofficially labeled .scl is valid SonicWeave if quoted
+          sourceLines.push(parts[0] + ' ' + JSON.stringify(parts.slice(1).join('')))
+        }
+      }
+    }
+    return { name, sourceText: sourceLines.join('\n') }
   }
 }
