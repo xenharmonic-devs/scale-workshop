6ea86c6072bcea928c9442c1b4ac03bec80f3b8d
Kiko Faxas <espiralinterna@gmail.com>
2025-02-06 11:00:23 +0100

    Add error message

--- src/components/ScoreView.vue
+++ src/components/ScoreView.vue
@@ -7,13 +7,17 @@ import Vex from 'vexflow';
 const state = useStateStore()
 const { Factory, BarlineType } = Vex.Flow;
 
-function isTrebleClef(chord: string){
+function isTrebleClef(chord: string | undefined){
+    if(!chord) return false
+
     const number = Number(chord.charAt(chord.length-1))
     if(number >= 4) return true
     return false
 }
 
-function isBassClef(chord: string){
+function isBassClef(chord: string | undefined){
+    if(!chord) return false
+    
     const number = Number(chord.charAt(chord.length-1))
     if(number < 4) return true
     return false
@@ -41,7 +45,13 @@ function refreshScore(chordArray: string[]){
     const score = vf.EasyScore()
     const system = vf.System({width: 160, spaceBetweenStaves: 8.5})
     const chordStringTreble = chordArray2chordString(chordArrayTreble)
-    const chordStringBass = chordArray2chordString(chordArrayBass)    
+    const chordStringBass = chordArray2chordString(chordArrayBass)
+    
+    // We just skipp if there is no notation   
+    if(!chordStringTreble && !chordStringBass) return
+  
+
+    
     try{
         if(chordStringTreble){
             system.addStave({
@@ -50,7 +60,7 @@ function refreshScore(chordArray: string[]){
                 ],
             }).addClef('treble').setEndBarType(BarlineType.DOUBLE);
         }
-
+        
         if(chordStringBass){
             system.addStave({
                 voices: [
@@ -62,13 +72,17 @@ function refreshScore(chordArray: string[]){
         vf.draw()
     } 
     catch(error){ 
-        //
+        const chordView = document.getElementById('chordView')
+        chordView.innerHTML = '<div class="error-msg">Oops!<br/><br/>The given symbol might not be a valid Vexflow label.</div>';
     }
 };
 
 watch(state.scoreChord, (newValue)=>{
     const chordView = document.getElementById('chordView')
     chordView.innerHTML = '';
+
+    if(!newValue[0]) return
+
     if(newValue.length > 0){
         refreshScore(newValue)
     }
@@ -82,17 +96,30 @@ watch(state.scoreChord, (newValue)=>{
 
 
 <style>
-#chordView {
+#chordView{
     position:fixed;
     z-index: 1;
     right: 6px;
     top: 10px;
 }
-
 svg{
     background-color: gainsboro;
     opacity: 90%;
     border: 2px solid lightslategray;
     border-radius: 15px;
 }
+
+.error-msg{
+    color: rgb(192, 14, 14);
+    background-color: gainsboro;
+    width: 160px;
+    height: 160px;
+    opacity: 90%;
+    border: 2px solid lightslategray;
+    border-radius: 15px;
+    padding: 15px;
+    text-align: center;
+    line-height: 1;
+}
+
 </style>
