6b6d9edc811c51f8ba0c88b0c59845f1e813eeb9
Kiko Faxas <espiralinterna@gmail.com>
2025-02-24 09:55:14 +0100

    Revert changes in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,6 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { version } from './package.json'
-
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -14,8 +12,5 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  },
-  define: {
-    'import.meta.env.PACKAGE_VERSION': JSON.stringify(version)
   }
 })
